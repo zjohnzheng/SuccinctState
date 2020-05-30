5f4e613568d94ce6f7bfa1aebe6866088e5c31dd
John <zjohnzheng@gmail.com>
2020-05-29 18:46:23 -0700

    Initial Commit

---
+++ SuccinctState/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SuccinctState
+//
+//  Created by Zheng on 5/29/20.
+//  Copyright © 2020 Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
